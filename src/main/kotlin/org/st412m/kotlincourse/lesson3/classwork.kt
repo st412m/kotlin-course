9ac5006d0d7833312e25006ddfca58a93bdc3935
st412m <staroverm@gmail.com>
2024-09-11 20:48:52 +0300

    settled on Available Equipment

--- src/main/kotlin/org/st412m/kotlincourse/lesson3/classwork.kt
+++ src/main/kotlin/org/st412m/kotlincourse/lesson3/classwork.kt
@@ -1,2 +1,15 @@
-package org.st412m.org.st412m.kotlincourse.lesson3
+package org.st412m.kotlincourse.lesson3
 
+val chassis: String = "123123ABC"
+var color: String = "Blue"
+var distance: Double = 0.0
+lateinit var owner: String
+const val WHEELS: Int = 4
+val report: String by lazy {
+    ""
+}
+var fuel: Double = 0.0
+   get() = field
+   set(value) {
+      if (value > 0) field += value
+}
