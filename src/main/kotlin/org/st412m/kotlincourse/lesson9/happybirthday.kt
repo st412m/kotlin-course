34eb33a6d76bf876e955b914a6a3d5360d5c2ee1
st412m <staroverm@gmail.com>
2024-10-02 15:34:27 +0300

    happy birthday

---
+++ src/main/kotlin/org/st412m/kotlincourse/lesson9/happybirthday.kt
@@ -0,0 +1,29 @@
+package org.st412m.kotlincourse.lesson9
+/*
+\uD83C\uDF82  - тортик
+\ud83d\udc90  - цветочки
+\ud83c\udf7e  - бухлишко
+\ud83d\udcb0  - деньжата
+\ud83c\udf81  - загадочный подарок
+ */
+
+
+fun main(){
+    happyBirthday("Александр", "\ud83c\udf81")  //впиши имя и добавь подарок
+}
+fun happyBirthday(name: String, emoji: String) {
+    var greeting = "С днем рождения"
+    if (name.isEmpty()) {
+        greeting = "$greeting!"
+    } else { greeting = "$greeting, ${name}!"
+    }
+    var border = ""
+    when {name.length < 7 -> border = emoji.repeat((greeting.length + 2) / emoji.length)
+        else -> border = emoji.repeat((greeting.length + 3) / emoji.length)}
+    println(border)
+    println("$emoji $greeting $emoji")
+    println(border)
+}
+
+// я без понятия как корректно рассчитать длину эмодзи, чтобы они ровно выстраивались, так что получите огород
+
