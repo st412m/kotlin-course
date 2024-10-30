053b4dd9d9d1db0d61173f2bc1c7ff533bfb6d6c
st412m <staroverm@gmail.com>
2024-10-30 10:53:31 +0300

    homework done

---
+++ src/main/kotlin/org/st412m/kotlincourse/lesson17/homework/Materials.kt
@@ -0,0 +1,84 @@
+package org.st412m.kotlincourse.lesson17.homework
+/*
+Это класс контейнера. Он абстрактный, это означает, что нельзя создавать экземлпяр этого класса, он нужен только
+для того, чтобы быть основой для подклассов.
+
+Создай подклассы этого контейнера, которые будут добавлять строку material в какое-то определённое место.
+Первый класс должен вставлять строку на дно контейнера (должно появиться под индексом 0)
+Второй класс должен получать список строк и вставлять их в начало контейнера но поочерёдно с теми данными,
+которые уже есть. То-есть, наш список должен появиться в контейнере по индексам 0, 2, 4 и так далее.
+Третий класс должен добавлять элементы в список в алфавитном порядке. Если мы добавляем новый элемент в список,
+то он должен встать где-то между другими элементами и занять место в соответствии с сортировкой по алфавиту.
+Четвёртый класс должен принимать словарь из строк (ключи и значения). Каждая пара ключа и значения должна попадать
+в контейнер следующим образом - ключ в начало, значение в конец.
+ */
+
+
+abstract class Materials {
+
+    private val materials = mutableListOf<String>()
+
+    fun addMaterial(material: String) {
+        materials.add(material)
+    }
+
+    fun extractMaterial(): List<String> {
+        val extracted = materials.toList()
+        materials.clear()
+        return extracted
+    }
+
+    fun printContainer() {
+        materials.forEachIndexed { index, layer ->
+            println("[$index]: $layer")
+        }
+    }
+}
+
+class MaterialsBottom(): Materials(){
+    fun addToBottom(material: String) {
+        val currentMaterials = extractMaterial().toMutableList()
+        addMaterial(material)
+        currentMaterials.forEach { addMaterial(it) }
+    }
+}
+
+class ThroughTheLine(): Materials(){
+    fun alternation(newMaterials: List<String>) {
+        val currentMaterials = extractMaterial()
+        val result = mutableListOf<String>()
+
+        var i = 0 // - новые штуки
+        var j = 0 // - старые штуки
+
+        while (i < newMaterials.size || j < currentMaterials.size) {
+            if (i < newMaterials.size) {
+                result.add(newMaterials[i])
+                i++
+            }
+            if (j < currentMaterials.size) {
+                result.add(currentMaterials[j])
+                j++
+            }
+        }
+        result.forEach { addMaterial(it) }
+    }
+}
+
+class Alphabetical : Materials() {
+    fun addAlphabetically(material: String) {
+        val currentMaterials = extractMaterial().toMutableList()
+        currentMaterials.add(material)
+        currentMaterials.sort()
+        currentMaterials.forEach { addMaterial(it) }
+    }
+}
+
+class KeyValue : Materials() {
+    fun addKeyValuePairs(pairs: Map<String, String>) {
+        val currentMaterials = extractMaterial()
+        pairs.keys.forEach { addMaterial(it) }
+        currentMaterials.forEach { addMaterial(it) }
+        pairs.values.forEach { addMaterial(it) }
+    }
+}
--- src/main/kotlin/org/st412m/kotlincourse/lesson17/homework/homework.kt
+++ src/main/kotlin/org/st412m/kotlincourse/lesson17/homework/homework.kt
@@ -1,2 +1,42 @@
 package org.st412m.kotlincourse.lesson17.homework
 
+fun main(){
+    val ordinalNumbers = listOf(
+        "first", "second", "third", "fourth", "fifth",
+        "sixth", "seventh", "eighth", "ninth", "tenth",
+        "eleventh", "twelfth", "thirteenth", "fourteenth", "fifteenth",
+        "sixteenth", "seventeenth", "eighteenth", "nineteenth", "twentieth"
+    )
+    println("----на дно-------")
+    val bottomContainer = MaterialsBottom()
+    ordinalNumbers.forEach { bottomContainer.addMaterial(it) }
+    bottomContainer.addToBottom("new какое-то что-то")
+    bottomContainer.printContainer()
+
+    println("----через строчку-------")
+    val newList = listOf(
+        "первый", "второй", "третий", "четвертый", "пятый",
+        "шестой", "седьмой", "восьмой", "девятый", "десятый",
+        "одиннадцатый", "двенадцатый", "тринадцатый", "четырнадцатый", "пятнадцатый",
+        "шестнадцатый", "семнадцатый", "восемнадцатый", "девятнадцатый", "двадцатый"
+    )
+
+    val throughTheLineContainer = ThroughTheLine()
+    ordinalNumbers.forEach { throughTheLineContainer.addMaterial(it) }
+    throughTheLineContainer.alternation(newList)
+    throughTheLineContainer.printContainer()
+
+    println("----по алфавиту-------")
+    val alphabetContainer = Alphabetical()
+    ordinalNumbers.forEach { alphabetContainer.addMaterial(it) }
+    alphabetContainer.addAlphabetically("new какое-то что-то")
+    alphabetContainer.printContainer()
+
+    println("----ключи/значения-------")
+    val pairs = mapOf("key1" to "value1", "key2" to "value2")
+    val keyValueContainer = KeyValue()
+    ordinalNumbers.forEach { keyValueContainer.addMaterial(it) }
+    keyValueContainer.addKeyValuePairs(pairs)
+    keyValueContainer.printContainer()
+
+}
