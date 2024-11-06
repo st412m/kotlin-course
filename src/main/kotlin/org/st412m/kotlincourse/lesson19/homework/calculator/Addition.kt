3858929951bd93088a054f6309629dbbb71dbf1b
st412m <staroverm@gmail.com>
2024-11-06 12:17:32 +0300

    calculator

--- src/main/kotlin/org/st412m/kotlincourse/lesson19/homework/calculator/Addition.kt
+++ src/main/kotlin/org/st412m/kotlincourse/lesson19/homework/calculator/Addition.kt
@@ -2,6 +2,7 @@ package org.st412m.kotlincourse.lesson19.homework.calculator
 
 class Addition : OperationClass() {
     override fun execute(operand1: Any, operand2: Any): String {
+        return TODO("Provide the return value")
     }
 
     override fun isApplicable(operand1: Any, operand2: Any): Boolean {
