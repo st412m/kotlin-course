df46c204db796cecd1dd45df0b0fd0405b0a834c
st412m <staroverm@gmail.com>
2024-11-07 10:40:25 +0300

    calculator

--- src/main/kotlin/org/st412m/kotlincourse/lesson19/homework/calculator/main.kt
+++ src/main/kotlin/org/st412m/kotlincourse/lesson19/homework/calculator/main.kt
@@ -6,7 +6,10 @@ fun main() {
     println(calculator.calculate("10.5 * 2"))
     println(calculator.calculate("вася + петя"))
     println(calculator.calculate("Hello * 3"))
+    println(calculator.calculate("Hello * 3.4"))
     println(calculator.calculate("5 % 2"))
+    println(calculator.calculate("5 % Петя"))
+    println(calculator.calculate("Петя % 5"))
     println(calculator.calculate("3 * Петя"))
     println(calculator.calculate("10 / Петя"))
     println(calculator.calculate("10 / 3"))
@@ -14,4 +17,6 @@ fun main() {
     println(calculator.calculate("1 + Петя"))
     println(calculator.calculate("10 - Петя"))
     println(calculator.calculate("10 : 1"))
+    println(calculator.calculate("++2"))
+    println(calculator.calculate("Петя - Вася"))
 }
