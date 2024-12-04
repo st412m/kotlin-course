59195da9f37ee37b424e7ccf9bcc69bb5cbdfe46
st412m <staroverm@gmail.com>
2024-12-04 13:39:41 +0300

    homework done

--- src/main/kotlin/org/st412m/kotlincourse/lesson26/homework/codeTasks.kt
+++ src/main/kotlin/org/st412m/kotlincourse/lesson26/homework/codeTasks.kt
@@ -1,7 +1,5 @@
 package org.st412m.kotlincourse.lesson26.homework
 
-import java.util.logging.Filter
-
 /*
 Напишите функцию filterStrings, которая принимает список строк и функцию-фильтр, оставляющую только строки,
 удовлетворяющие условию (то-есть принимающая строку и возвращающая булево значение). Функция должна вернуть результат
@@ -34,13 +32,28 @@ fun applyToNumbers(
 в сумму. Функция должна вернуть сумму чисел, прошедших проверку.
  */
 
-
+fun sumByCondition(
+    numList: List<Number>,
+    conditionDefinition: (Number) -> Boolean
+): Double {
+    return numList.filter(conditionDefinition).sumOf{it.toDouble()}
+}
 
 
 /*
 Напишите функцию combineAndTransform, которая принимает две коллекции одного типа и функцию для их объединения и
 преобразования в одну коллекцию другого типа. Функция должна вернуть результат преобразования (коллекцию второго типа)
  */
+
+fun combineAndTransform(
+    collectionOne: List<String>,
+    collectionTwo: List<Int>,
+    convertToMap: (List<String>, List<Int>) -> Map<String, Int>
+
+): Map<String, Int>{
+    return convertToMap(collectionOne, collectionTwo)
+}
+
 
 fun main() {
     println("\n----- Задача 1 -----")
@@ -53,4 +66,48 @@ fun main() {
     val numList2 = listOf(1.0, 2.0, -1.0, 5.0, -10.0)
     println(applyToNumbers(numList1){it.toDouble()})
     println(applyToNumbers(numList2){it.toInt() * 2})
+
+    println("\n----- Задача 3 -----")
+    val numList3 = listOf(1, -2, 3, -4, 5, -6)
+    val numList4 = listOf(1.0, -2.0, 3.01, -4.06, 5.0, -6.0)
+    println(sumByCondition(numList3){it.toDouble() > 0})
+    println(sumByCondition(numList4){it.toString().length < 4})
+
+    println("\n----- Задача 4 -----")
+    val collectionOne = listOf("один", "два", "три", "четыре")
+    val collectionTwo = listOf(1, 2, 3, 4)
+    val collectionThree = listOf("один", "два", "три", "четыре", "пять")
+    val collectionFour = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+
+    val result = combineAndTransform(collectionOne, collectionTwo) { list1, list2 ->
+        val map = mutableMapOf<String, Int>()
+        val size = minOf(list1.size, list2.size)
+        for (i in 0 until size) {
+            map[list1[i]] = list2[i]
+        }
+        map
+    }
+
+    println(result)
+
+    val anotherResult = combineAndTransform(collectionThree, collectionFour) { list1, list2 ->
+        val map = mutableMapOf<String, Int>()
+        val keyListSize = list1.size
+
+        for (i in list2.indices) {
+            val key = if (i < keyListSize) {
+                list1[i]
+            } else {
+                list1[i % keyListSize] + "-" + list2[i]
+            }
+            map[key] = list2[i]
+        }
+        map
+    }
+    println(anotherResult)
 }
+
+
+
+
