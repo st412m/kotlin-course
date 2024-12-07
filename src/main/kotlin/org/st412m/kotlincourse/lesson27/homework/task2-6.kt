83b1e5f565aebccf7d0f32af2f9bbd94160a12eb
st412m <staroverm@gmail.com>
2024-12-07 09:41:33 +0300

    task5

--- src/main/kotlin/org/st412m/kotlincourse/lesson27/homework/task2-6.kt
+++ src/main/kotlin/org/st412m/kotlincourse/lesson27/homework/task2-6.kt
@@ -17,37 +17,43 @@ fun main() {
     println("Department: ${employee1.department}")
 
     println("\n-----Задача 3------")
-/*
-Создайте функцию, принимающую объект Person и выводящую информацию об объекте в консоль в красивом, отформатированном виде.
-Создайте объект Person и используйте also для печати данных в консоль с помощью метода который только что создали.
- */
+    /*
+    Создайте функцию, принимающую объект Person и выводящую информацию об объекте в консоль в красивом, отформатированном виде.
+    Создайте объект Person и используйте also для печати данных в консоль с помощью метода который только что создали.
+     */
 
     val person1 = Person("Адам", 30).apply {
         email = "firstman@genesis.net"
-    }.also{println(niceOutput(it))}
+    }.also { println(niceOutput(it)) }
 
     println("\n-----Задача 4------")
-/*
-Создайте объект класса Person. Используйте функцию with, чтобы создать из этого объекта Employee,
-указав дополнительно должность. Возвращаемым значением должен быть новый объект Employee.
- */
+    /*
+    Создайте объект класса Person. Используйте функцию with, чтобы создать из этого объекта Employee,
+    указав дополнительно должность. Возвращаемым значением должен быть новый объект Employee.
+     */
 
     val person2 = Person("Ева", 25).apply {
         email = "firstwoman@genesis.net"
     }
-    val personHiring = with(person2){
+    val personHiring = with(person2) {
         Employee(name, age, "Посудомойка")
     }
 
     println("${personHiring.name}\n${personHiring.age}\n${personHiring.position}")
 
     println("\n-----Задача 5------")
-/*
-Создайте объект класса Person. Используйте функцию run, чтобы инициализировать объект Employee, добавить ему email
-и department с помощью apply, вернуть инициализированный объект.
- */
+    /*
+    Создайте объект класса Person. Используйте функцию run, чтобы инициализировать объект Employee, добавить ему email
+    и department с помощью apply, вернуть инициализированный объект.
+     */
+    val person3 = Person("Каин", 2).run {
+        Employee(name, age, "Пульт от телека").apply {
+            email = "firstson@genesis.net"
+            department = "Счастливая семья"
+        }
+    }
 
-
+    println("${person3.name}\n${person3.age}\n${person3.email}\n${person3.department}")
 
 }
 
