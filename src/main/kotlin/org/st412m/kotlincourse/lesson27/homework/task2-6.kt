3f618a42503ffb34b812e0503ae422f9044d4026
st412m <staroverm@gmail.com>
2024-12-07 10:56:16 +0300

    homework done

--- src/main/kotlin/org/st412m/kotlincourse/lesson27/homework/task2-6.kt
+++ src/main/kotlin/org/st412m/kotlincourse/lesson27/homework/task2-6.kt
@@ -1,10 +1,13 @@
 package org.st412m.kotlincourse.lesson27.homework
 
+import kotlin.let
+import kotlin.math.E
+
 
 fun main() {
     /*
-Создайте объект Employee и используйте apply для инициализации его полей email и department.
-*/
+    Создайте объект Employee и используйте apply для инициализации его полей email и department.
+    */
     println("\n-----Задача 2------")
     val employee1 = Employee("Иисус Христос", 33, "сын божий").apply {
         email = "jesus@heaven.com"
@@ -42,11 +45,12 @@ fun main() {
     println("${personHiring.name}\n${personHiring.age}\n${personHiring.position}")
 
     println("\n-----Задача 5------")
+
     /*
     Создайте объект класса Person. Используйте функцию run, чтобы инициализировать объект Employee, добавить ему email
     и department с помощью apply, вернуть инициализированный объект.
      */
-    val person3 = Person("Каин", 2).run {
+    val person3 = Person("Каин", 20).run {
         Employee(name, age, "Пульт от телека").apply {
             email = "firstson@genesis.net"
             department = "Счастливая семья"
@@ -55,10 +59,32 @@ fun main() {
 
     println("${person3.name}\n${person3.age}\n${person3.email}\n${person3.department}")
 
+    println("\n-----Задача 6------")
+    /*
+    Создайте функцию, которая принимает объект класса Person?, который может быть null.
+    В функции используйте функцию let для создания Employee, только если объект Person не null.
+    В противном случае возвращается null
+     */
+
+    val person4: Person? = Person("Авель", 10)
+    val person5: Person? = null
+
+    forSlaughter(person4)
+    println(forSlaughter(person5))
+
 }
 
 fun niceOutput(obj: Person): String {
     return "Имя: ${obj.name}\nВозраст: ${obj.age}\nemail: ${obj.email}"
 }
 
-
+fun forSlaughter(obj: Person?): Employee? {
+    return obj?.let {
+        Employee(it.name, it.age, "будущая жертва").apply {
+            email = "firstson@genesis.net"
+            department = "Счастливая семья"
+        }.also {
+            println("${it.name}\n${it.age}\n${it.position}\n${it.email}\n${it.department}")
+        }
+    }
+}
