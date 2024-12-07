9fb5dfa9d224426e7d933bd7945c5942754eb435
st412m <staroverm@gmail.com>
2024-12-07 08:29:06 +0300

    homrwork task1

---
+++ src/main/kotlin/org/st412m/kotlincourse/lesson27/homework/task1.kt
@@ -0,0 +1,51 @@
+package org.st412m.kotlincourse.lesson27.homework
+
+/*
+Создайте функцию timeTracker, которая принимает другую функцию в качестве аргумента и измеряет время её выполнения.
+Функция timeTracker должна возвращать затраченное время в миллисекундах. Для измерения времени
+используйте System.currentTimeMillis() до и после выполнения переданной функции.
+ */
+
+
+fun timeTracker(func: () -> List<Int>): String {
+    val startTime = System.currentTimeMillis()
+    val result = func()
+    val endTime = System.currentTimeMillis()
+    val executionTime = endTime - startTime
+    return "Результат работы функции: $result\nВремя выполнения: $executionTime ms\n"
+}
+
+fun timeTrackerForSting(func: () -> Unit): String {
+    val startTime = System.currentTimeMillis()
+    func()
+    val endTime = System.currentTimeMillis()
+    val executionTime = endTime - startTime
+    return "Время выполнения: $executionTime ms"
+}
+
+fun main() {
+    val myFunction = {
+        val list = List(10000000) { (0..10000).random() }
+        list.sorted()
+    }
+
+    println(timeTracker(myFunction))
+
+    val stringFunc = {
+        var s = ""
+        for (i in 0..20000) {
+            s += "новая запись"
+        }
+    }
+
+    println(timeTrackerForSting(stringFunc))
+
+    val stringBuilderFunc = {
+        val s = StringBuilder()
+        for (i in 0..20000) {
+            s.append("новая запись")
+        }
+    }
+    println(timeTrackerForSting(stringBuilderFunc))
+
+}
