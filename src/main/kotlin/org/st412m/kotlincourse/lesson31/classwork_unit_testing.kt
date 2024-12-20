050e8629c03eb44d1bc46111cc3ddbc30896063a
st412m <staroverm@gmail.com>
2024-12-20 21:14:03 +0300

    Unit testing

--- src/main/kotlin/org/st412m/kotlincourse/lesson31/classwork_unit_testing.kt
+++ src/main/kotlin/org/st412m/kotlincourse/lesson31/classwork_unit_testing.kt
@@ -4,3 +4,15 @@ class Calculator() {
 
     fun plus(a: Int, b: Int) = a + b
 }
+/*
+засунуть это в build.gradle.kts в
+testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("com.google.code.gson:gson:2.10.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+}
+ */
