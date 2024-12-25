7ec5dd18b6b31f8919f3d62ac0845a74aafd1480
st412m <staroverm@gmail.com>
2024-12-25 07:49:29 +0300

    classwork update

--- src/test/kotlin/org/st412m/kotlincourse/lesson32/TextToPhoneTest.kt
+++ src/test/kotlin/org/st412m/kotlincourse/lesson32/TextToPhoneTest.kt
@@ -1,14 +1,14 @@
 package org.st412m.kotlincourse.lesson32
 
-import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 
 class TextToPhoneTest {
 
     @ParameterizedTest
-    @MethodSource("providePhoneNumber")
+    @MethodSource("providePhoneNumbers")
     fun `check convertation`(
         phoneNumber: String,
         expected: String
@@ -23,11 +23,9 @@ class TextToPhoneTest {
     companion object {
         @JvmStatic
         fun providePhoneNumbers() = listOf(
-            arrayOf("81234567890", "+7 (123) 456-78-90"),
-            arrayOf("8(123)4567890 Вася", "+7 (123) 456-78-90"),
-            arrayOf("7(123)4567890", "+7 (123) 456-78-90")
+            Arguments.of("81234567890", "+7 (123) 456-78-90"),
+            Arguments.of("8(123)4567890 Вася", "+7 (123) 456-78-90"),
+            Arguments.of("7(123)4567890", "+7 (123) 456-78-90")
         )
     }
-
-}
 }
